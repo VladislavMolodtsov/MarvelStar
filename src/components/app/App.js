bd58ec9d775f31998e445887b7c576f8296ee8d9
VladislavMolodtsov <vladi28061750@gmail.com>
2024-01-08 22:33:46 +0200

    using React.lazy, Suspense for dinamic import components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,15 @@
 import {lazy, Suspense} from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+// React.lazy() позволяет определять компонент, который загружается динамически. Это позволяет уменьшить размер сборки, откладывать загрузку на потом
+// ... React.lazy() будет загружать в bundl компонент только тогда, когда компонент будет впервые отрендерен ...
+// ... Например зашли мы первый раз на комикс, только тогда компонент загрузится с одним важным условием, компонент должен быть экспортирован по умолчанию
+// Обязательно компонент который должен загрузится динамически должен быть экспортирован по умолчанию
+
 import AppHeader from "../appHeader/AppHeader";
 import Spinner from '../spinner/Spinner';
 
+// Мы должны подгружать все динамические import после статических, иначе может возникнуть ошибка
 // import { MainPage, ComicsPage, Page404, SingleComic } from "../pages/index";
 const MainPage = lazy(() => import('../pages/MainPage'));
 const ComicsPage = lazy(() => import('../pages/ComicsPage'));
@@ -14,6 +20,8 @@ const App = () => {
     
     return (
         <Suspense fallback={<Spinner/>}>
+        {/* Suspense есть обязательный аттрибут fallback={} - он нужен чтобы показать запасной компонент пока грузится динамический import */}
+        {/* В него можно поместить react компонент и react элемент */}
             <Router>
                 <div className="app">
                     <AppHeader/>
